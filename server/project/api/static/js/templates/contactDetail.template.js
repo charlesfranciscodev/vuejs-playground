2f0a81aa306a65f3ae38bacbf0e3ca7a74b29304
Charles-Antoine Francisco <charlantfr@gmail.com>
2019-11-07 17:44:18 -0500

    View Contact Route

--- server/project/api/static/js/templates/contactDetail.template.js
+++ server/project/api/static/js/templates/contactDetail.template.js
@@ -4,19 +4,21 @@ const ContactDetailTemplate = `
     <div class="col-8">
       <div class="text-center">
         <h2>{{ fullName }}</h2>
-          <p class="font-weight-bold">{{ age }} years old</p>
-          <p class="font-italic">
-            <i class="fas fa-envelope"></i> 
-            {{ contact.email }}
-          </p>
-          <p class="font-italic">
-            <i class="fas fa-phone"></i> 
-            {{ contact.phone_number }}
-          </p>
-        </div>
-        <h3>Description</h3>
-        <p>{{ contact.description }}</p>
+        <p class="font-weight-bold">{{ age }} years old</p>
+        <p class="font-italic">
+          <i class="fas fa-envelope"></i> 
+          {{ contact.email }}
+        </p>
+        <p class="font-italic">
+          <i class="fas fa-phone"></i> 
+          {{ contact.phone_number }}
+        </p>
+      </div>
+
+      <h3>Description</h3>
+      <p>{{ contact.description }}</p>
     </div>
+
     <div class="col-4">
       <img :src="contact.avatar_url" class="img-thumbnail" :alt="fullName">
     </div>
